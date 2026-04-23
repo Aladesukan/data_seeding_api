9d7b87dc5f88bee2dfade7b5951709c061063d17
Aladesukan <aladesukanf@gmail.com>
2026-04-23 18:12:02 +0100

    add migrate route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +11,8 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/profiles',[ProfileController::class,'index']);
 Route::get('/profiles/search', [ProfileController::class, 'search']);
+
+Route::get('/migrate', function () {
+    \Artisan::call('migrate', ['--force' => true]);
+    return 'Migration done';
+});
